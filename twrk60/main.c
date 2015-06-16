a5ba13b2100e639969b62ebf1852e0ac22d8a4b0
David Muriuki <karfes@gmail.com>
2015-06-16 19:18:08 +0300

    Added a baremetal GPIO example Added basic startup and linker scripts, header files and a makefile Added a .gdbcmds gdb commands file and a debug make target: use 'make debug' to debug Added documentation files in the 'Documentation' folder ignored the output folder in .gitignore :P

---
+++ twrk60/main.c
@@ -0,0 +1,50 @@
+/*
+ * File: main.c
+ *
+ * Copyright (c) karibe 2015, karfes@gmail.com
+ *
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://www.wtfpl.net/ for more details.
+ *
+ */
+
+#include "main.h"
+
+int main(void){	
+	//whether to blink LEDS
+	blink=1;
+
+	//initialize system
+	SystemInit();
+
+	//clear all interrupts and enable interrupts
+	nvic->ICPR[2] |= 1 << (87 & 0x1F); //Clear Pending Interrupts
+	nvic->ISER[2] |= 1 << (87 & 0x1F); //Enable Interrupts
+	
+	//initialize GPIO ports
+	gpio_init();
+
+	//Loop forever
+	while(1)
+	{
+		if(blink) toggle_LED2();
+		delay();
+	}
+}
+
+/*
+	brief  Silly delay
+*/
+void delay(void)
+{
+  volatile unsigned int i,j;
+
+  for(i=0; i<25000; i++)
+  {
+	for(j=0; j<300; j++)
+      __asm__("nop");
+  }
+}
