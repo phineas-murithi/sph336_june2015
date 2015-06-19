7b16c30c9712dd090b6abfb6ceebc07c6a4c44a8
mulan-94 <amwayilexy.me@gmail.com>
2015-06-19 15:41:45 +0300

    I have edited the outputs of this program as the initial one was faulty

--- example1/main.c
+++ example1/main.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main (int argc, char* argv[]){
-	char a = atoi(argv[1]);
-	printf("the value of a was %d\n", a);
-	return 0;
+#include <string.h>
+int main (int argc , char* argv[])
+/*arg c is used to show the number of inputs  one put when running the program.
+eg ./main.o will give an output of 1 while ./main.o lexy Andati will give an output
+of 3 because you have input 3 things while  running the executable file*/
+/* in c language char* means a string*/
+{
+//int a= atoi(argv[1]);
+//printf("The value of a was %d \n", a);
+//printf("argc = %d, %s\n", argc, argv[argc-3]);
+printf("The value of argc is %d value of argv[1] %s and integer value of argv[1] is  \n", argc, argv[1]);
+return 0;
 }
+
