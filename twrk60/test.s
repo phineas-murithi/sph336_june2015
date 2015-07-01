f956800495e236196a565d1b829872a58cfec77b
ChelmisMT <tchelmis07@gmail.com>
2015-07-01 19:16:56 +0300

    added toggle_LED4 function

--- twrk60/test.s
+++ twrk60/test.s
@@ -12,9 +12,11 @@
 
 .equ LEDS, 1<<11 | 1<<28 | 1<<29 | 1<<10	//declare the LEDS mask
 .equ LED2, 1<<28	//declare LED2 mask
+.equ LED4, 1<<10	//declare LED4 mask
 
 .global toggle_LEDS
 .global toggle_LED2
+.global toggle_LED4
 
 toggle_LEDS:
 	STMDB SP!, {R4-R11}	// Push 8 "longs" on the stack and subtracts sp beforehand
@@ -39,5 +41,14 @@ toggle_LED2:
 	MOV R0, #0
 	MOV PC, LR
 
-
+toggle_LED4:
+	STMDB SP!, {R4-R11} /* Push 8 "longs" on the stack and subtracts sp beforehand */
+	LDR R1, =0x400ff00c// 	create a pointer to GPIOA_PTOR
+	LDR R2, =LED4//				load the value of c into R2
+	LDR R3, [R1]//			Load GPIOA_PTOR using the pointer
+	ORR R3, R2//				mask the value from GPIOA_PTOR with value from c
+	STR R3, [R1]//			Store back the
+	LDMIA SP!, {R4-R11}
+	MOV R0, #0
+	MOV PC, LR
 
